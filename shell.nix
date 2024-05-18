0151e669ee87c280c356638f588d6fe3063c8c0f
alexkharl <sasha_xarlamov@mail.ru>
2024-05-18 20:54:27 +0300

    Repo:Доб-ие среды

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ sources ? import ./nix/sources.nix }:
+let
+  pkgs = import sources.nixpkgs { config = {}; overlays = []; };
+  pyEnv = pkgs.python3.withPackages (ps: with ps; [
+    torch
+  ]);
+  shell = pkgs.mkShellNoCC {
+    inputsFrom = [ ];
+    packages = with pkgs; [
+      pyEnv
+    ];
+
+    hardeningDisable = ["all"];
+  };
+in {
+  inherit shell;
+}
