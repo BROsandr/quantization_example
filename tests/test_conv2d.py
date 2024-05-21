e7ae9d2f79154c44578df19bb5846faa1d9a3fed
brosandr <sasha_xarlamov@mail.ru>
2024-05-21 13:53:02 +0300

    Feat(tests/conv2d):Допил-ие mymatmul

--- tests/test_conv2d.py
+++ tests/test_conv2d.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import random
 import os
 from broquant.QTensor import dequantize_tensor, quantize_tensor
-from typing import Sequence
+from typing import Iterable, Sequence
 
 import logging
 logger = logging
@@ -27,20 +27,57 @@ class TestMyConv2d(unittest.TestCase):
 
     inp_unf = torch.nn.functional.unfold(input=input, kernel_size=weight.shape[-2:], padding=padding, stride=stride)
 
-    tmp_inp = inp_unf.transpose(1, 2)
-    tmp_weight = weight.view(weight.size(0), -1).t()
+    def mymatmul(input: torch.Tensor, other: torch.Tensor)->torch.Tensor:
+      """
+        Partial implementation of torch.matmul. Utilizes torch.mm.
+      """
 
-    def mymatmul(input: torch.Tensor, other: torch.Tensor):
-      assert_shape = lambda tensor: 2 <= len(tensor.shape) <= 4
-      assert(assert_shape(input) and assert_shape(other))
+      is_valid_shape_len = lambda tensor: 2 <= len(tensor.shape) <= 4
+      if not(is_valid_shape_len(input) and is_valid_shape_len(other)):
+        raise ValueError(f'Unsupported shape. input.shape:{input.shape}, other.shape:{other.shape}')
 
-      get_normalized_shape = lambda tensor: (((1,) * 4) + tuple(tensor.shape))[-4:]
+      def normalize_shape(tensor: torch.Tensor)->int:
+        num_of_extra_dims = 0
+        while len(tensor.shape) != 4:
+          tensor.unsqueeze_(0) # prepend dimension
+          num_of_extra_dims += 1
+        return num_of_extra_dims
 
-      input_norm_shape = get_normalized_shape(input)
-      other_norm_shape = get_normalized_shape(other)
+      def denormalize_shape(tensor: torch.Tensor, num_of_extra_dims)->None:
+        for _ in range(num_of_extra_dims): tensor.squeeze_(0)
 
+      inp_num_of_extra_dims = normalize_shape(input)
+      other_num_of_extra_dims = normalize_shape(other)
 
-    out_unf = inp_unf.transpose(1, 2).matmul(weight.view(weight.size(0), -1).t()).transpose(1, 2)
+      from functools import partial
+
+      denormalize_shape = partial(denormalize_shape, num_of_extra_dims=min(inp_num_of_extra_dims, other_num_of_extra_dims))
+
+      are_dims_compatible = lambda dim1, dim2: (dim1 == dim2) or (dim1 == 1 or dim2 == 1)
+      assert(all([are_dims_compatible(inp_dim, other_dim) for inp_dim, other_dim in zip(input.shape, other.shape)][0:2]))
+
+      def get_broadcast_indices(dim1: int, dim2: int)->Iterable[tuple[int, int]]:
+        from itertools import zip_longest
+        return zip_longest(range(dim1), range(dim2), fillvalue=0) # for 1-dims an index will always be 0
+
+      batch_out: list[list[torch.Tensor]] = []
+      for inp_batch_idx, other_batch_idx in get_broadcast_indices(input.shape[0], other.shape[0]):
+        channel_out: list[torch.Tensor] = []
+        for inp_chan_idx, other_chan_idx in get_broadcast_indices(input.shape[1], other.shape[1]):
+          channel_out.append(input[inp_batch_idx, inp_chan_idx, ...].mm(other[other_batch_idx, other_chan_idx, ...]))
+        batch_out.append(channel_out)
+
+      def cat_tensors(seq: Sequence)->torch.Tensor:
+        if all(isinstance(el, torch.Tensor) for el in seq):
+          return torch.stack(seq)
+        return cat_tensors([cat_tensors(el) for el in seq])
+
+      out = cat_tensors(batch_out)
+      denormalize_shape(out)
+
+      return out
+
+    out_unf = mymatmul(inp_unf.transpose(1, 2), (weight.view(weight.size(0), -1).t())).transpose(1, 2)
 
     # see h_out, w_out formulas in https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html#torch.nn.Conv2d
     h_out = floor((input.shape[-2] + 2 * padding[0] - dilation[0] * (weight.shape[-2] - 1) - 1) / stride[0] + 1)
