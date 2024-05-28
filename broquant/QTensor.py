659f643ca36c812663b559345277a8d1f9a70c46
alexkharl <sasha_xarlamov@mail.ru>
2024-05-28 21:50:49 +0300

    Feat(QTensor):Доб-ие matmul

--- broquant/QTensor.py
+++ broquant/QTensor.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Sequence, Iterable
 import torch
 import logging
 logger = logging
@@ -50,7 +50,8 @@ class QTensor(torch.Tensor):
         raise NotImplementedError(f'QTensors have different scale/zp in func:{func}. Provide a specific handler if want to apply the func.')
       res = super().__torch_function__(func, types, args, kwargs)
       if isinstance(res, QTensor):
-        res = args[0].clone(new_tensor=res)
+        arg = args[0][0] if isinstance(args[0], list) else args[0] # args[0] is list of QTensor for the func stack()
+        res = arg.clone(new_tensor=res)
       return res
     return _HANDLED_FUNCTIONS[func](*args, **kwargs)
 
@@ -149,3 +150,54 @@ def q_mm(input: QTensor, mat2: QTensor)->QTensor:
       for j in range(bc):
           c[i,j] = (input[i,:] * mat2[:,j]).sum(dtype=c.dtype) # multiply all of column j by all of row i and sum it
   return c
+
+@implements(torch.matmul)
+def q_matmul(input: torch.Tensor, other: torch.Tensor)->torch.Tensor:
+  """
+    Partial implementation of torch.matmul. Utilizes torch.mm.
+  """
+
+  is_valid_shape_len = lambda tensor: 2 <= len(tensor.shape) <= 4
+  if not(is_valid_shape_len(input) and is_valid_shape_len(other)):
+    raise ValueError(f'Unsupported shape. input.shape:{input.shape}, other.shape:{other.shape}')
+
+  def normalize_shape(tensor: torch.Tensor)->int:
+    num_of_extra_dims = 0
+    while len(tensor.shape) != 4:
+      tensor.unsqueeze_(0) # prepend dimension
+      num_of_extra_dims += 1
+    return num_of_extra_dims
+
+  def denormalize_shape(tensor: torch.Tensor, num_of_extra_dims)->None:
+    for _ in range(num_of_extra_dims): tensor.squeeze_(0)
+
+  inp_num_of_extra_dims = normalize_shape(input)
+  other_num_of_extra_dims = normalize_shape(other)
+
+  from functools import partial
+
+  denormalize_shape = partial(denormalize_shape, num_of_extra_dims=min(inp_num_of_extra_dims, other_num_of_extra_dims))
+
+  are_dims_compatible = lambda dim1, dim2: (dim1 == dim2) or (dim1 == 1 or dim2 == 1)
+  assert(all([are_dims_compatible(inp_dim, other_dim) for inp_dim, other_dim in zip(input.shape, other.shape)][0:2]))
+
+  def get_broadcast_indices(dim1: int, dim2: int)->Iterable[tuple[int, int]]:
+    from itertools import zip_longest
+    return zip_longest(range(dim1), range(dim2), fillvalue=0) # for 1-dims an index will always be 0
+
+  batch_out: list[list[torch.Tensor]] = []
+  for inp_batch_idx, other_batch_idx in get_broadcast_indices(input.shape[0], other.shape[0]):
+    channel_out: list[torch.Tensor] = []
+    for inp_chan_idx, other_chan_idx in get_broadcast_indices(input.shape[1], other.shape[1]):
+      channel_out.append(input[inp_batch_idx, inp_chan_idx, ...].mm(other[other_batch_idx, other_chan_idx, ...]))
+    batch_out.append(channel_out)
+
+  def cat_tensors(seq: tuple | list)->torch.Tensor:
+    if all(isinstance(el, torch.Tensor) for el in seq):
+      return torch.stack(seq)
+    return cat_tensors([cat_tensors(el) for el in seq])
+
+  out = cat_tensors(batch_out)
+  denormalize_shape(out)
+
+  return out
